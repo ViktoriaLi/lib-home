0d67f0a8f5d622eb9e7bcabb6d74fb5bd1d3eaba
Viktoriia LIKHOTKINA <vlikhotk@e2r14p3.unit.ua>
2017-11-21 14:13:26 +0200

    Try to fix lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -16,37 +16,25 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list *new_list;
-	t_list *tmp;
+	t_list *new;
+	t_list *temp;
 
+	new = NULL;
+	temp = NULL;
 	if (lst && f)
-	{
-		while (lst)
-		{
-			f(lst);
-			tmp = new_list;
-			new_list = ft_lstnew(lst->content, lst->content_size);
-			lst = lst->next;
-			free(tmp);
-		}
-		return (new_list);
-	}
-	return (NULL);
-}
-
-/*
-
-	t_list *new_list;
-
+		return (NULL);
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new = f(lst);
+	lst = lst->next
 	while (lst)
 	{
-		if (!(new_list = (t_list *)malloc(sizeof(t_list))))
+		if (!(new = ft_lstnew(lst->content, lst->content_size)))
 			return (NULL);
-		new_list = f(lst);
-		new_list = new_list->next;
-		lst = lst->next;
+		new = f(lst);
+		temp = new;
+		new = new->next;
+		lst = lst->next
 	}
-	new_list = NULL;
-	return (new_list);
+	return (new);
 }
-*/
